ae3abb927dfbd7a45d13ba0a213093fdb728926b
vtrinh1 <vincent.trinh1@hotmail.com>
2021-09-19 15:05:58 +1000

    initial commit

---
+++ src/components/News.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
+import moment from 'moment'
+
+function News() {
+  return (
+    <div>
+      News
+    </div>
+  )
+}
+
+export default News
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'cb43fafedamshdf248f6d915246ap106f6fjsna2399cc3d057'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`)
+    })
+  })
+})
+
+export const {
+  useGetCryptosQuery
+} = cryptoApi
